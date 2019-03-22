7d31abc466f815b017f0089812dd8d70366cc93e
ZinchenkoVasil <zinchenkovasil@mail.ru>
2019-03-22 15:22:03 +0300

    новая дополненная версия контроллера

--- hw03/Server/text/controllers.py
+++ hw03/Server/text/controllers.py
@@ -1,11 +1,16 @@
 #ответ сервера на запрос установления соединения
-
+from Server.decorators import *
 
 #def get_upper_text(data):
 #    return data.upper()
-
+@log
 def get_response(account_name):
+    test()
     if account_name:
         return "Hello, " + account_name
     else:
         return "Hello"
+
+@log
+def test():
+    pass
